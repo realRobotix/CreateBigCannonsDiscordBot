b8eab808add88aa282ea4494cb9ecd7705e5a1fe
Robotix <82544307+realRobotix@users.noreply.github.com>
2023-01-11 09:51:36 +0100

    HOTFIX: add error handling to fix duplicate reposts

--- extensions/stars/stars.py
+++ extensions/stars/stars.py
@@ -26,57 +26,59 @@ class Stars(commands.Cog):
             and event.channel_id == self.bot.env.STARS_VOTE_CHANNEL_ID
             and event.message_id not in self.reposted
         ):
-            channel = self.bot.get_channel(event.channel_id)
-            message = await channel.fetch_message(event.message_id)
-            if message.author == self.bot.user:
-                return
-            reactions = message.reactions
-            for reaction in reactions:
-                if reaction.emoji == "⭐" and (
-                    (
-                        reaction.count >= self.bot.env.STARS_THRESHOLD
-                        and (message.embeds != None or message.attachments != None)
-                    )
-                    or await reaction.users().find(lambda u: u.id in self.bot.env.STARS_BYPASS) != None
-                ):
-                    # repost the message within an embed to the repost channel
-                    repost_channel = self.bot.get_channel(self.bot.env.STARS_REPOST_CHANNEL_ID)
-
-                    first_embed = (
-                        disnake.Embed(
-                            color=disnake.Colour.gold(),
-                            url=message.jump_url,
+                channel = self.bot.get_channel(event.channel_id)
+                message = await channel.fetch_message(event.message_id)
+                if message.author == self.bot.user:
+                    return
+                reactions = message.reactions
+                for reaction in reactions:
+                    if reaction.emoji == "⭐" and (
+                        (
+                            reaction.count >= self.bot.env.STARS_THRESHOLD
+                            and (message.embeds != None or message.attachments != None)
                         )
-                        .set_author(name=message.author.display_name, icon_url=message.author.avatar.url)
-                        .set_footer(text=event.message_id)
-                        .add_field(
-                            name="Original message",
-                            value=f"[Jump to message]({message.jump_url})",
-                            inline=False,
-                        )
-                    )
+                        or await reaction.users().find(lambda u: u.id in self.bot.env.STARS_BYPASS) != None
+                    ):
+                        # repost the message within an embed to the repost channel
+                        repost_channel = self.bot.get_channel(self.bot.env.STARS_REPOST_CHANNEL_ID)
 
-                    if message.content != None and message.content != "":
-                        first_embed.add_field(
-                            name="Message",
-                            value=message.content,
+                        first_embed = (
+                            disnake.Embed(
+                                color=disnake.Colour.gold(),
+                                url=message.jump_url,
+                            )
+                            .set_author(name=message.author.display_name, icon_url=message.author.avatar.url)
+                            .set_footer(text=event.message_id)
+                            .add_field(
+                                name="Original message",
+                                value=f"[Jump to message]({message.jump_url})",
+                                inline=False,
+                            )
                         )
 
-                    if len(message.attachments) > 0:
-                        for i, attachment in enumerate(message.attachments):
-                            first_embed.add_field(name=f"Attachment {i + 1}", value=attachment.url, inline=False)
-                        for i, attachment in enumerate(message.attachments):
-                            if attachment.content_type != None and attachment.content_type.startswith("image"):
-                                first_embed.set_image(url=message.attachments.pop(i).url)
-                                break
+                        if message.content != None and message.content != "":
+                            first_embed.add_field(
+                                name="Message",
+                                value=message.content,
+                            )
 
-                    embeds, other_attachments = await self.make_image_embeds(message.attachments)
-                    embeds.insert(0, first_embed)
+                        if len(message.attachments) > 0:
+                            for i, attachment in enumerate(message.attachments):
+                                first_embed.add_field(name=f"Attachment {i + 1}", value=attachment.url, inline=False)
+                            for i, attachment in enumerate(message.attachments):
+                                if attachment.content_type != None and attachment.content_type.startswith("image"):
+                                    first_embed.set_image(url=message.attachments.pop(i).url)
+                                    break
 
-                    await repost_channel.send(embeds=embeds)
-                    if other_attachments != None and other_attachments != []:
-                        await repost_channel.send(files=other_attachments)
-                    self.reposted.add(event.message_id)
+                        embeds, other_attachments = await self.make_image_embeds(message.attachments)
+                        embeds.insert(0, first_embed)
+
+                        try:
+                            await repost_channel.send(embeds=embeds)
+                            if other_attachments != None and other_attachments != []:
+                                await repost_channel.send(files=other_attachments)
+                        finally:
+                            self.reposted.add(event.message_id)
 
     async def make_image_embeds(
         self,
