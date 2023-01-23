a5c2b81e3b0ae4c5e6b2ed4b59122fc50dc26223
Julian Haarmann <julian.h.haarmann@gmail.com>
2023-01-23 22:02:25 +0100

    set stars limit to 25 and add 7 day limit

--- extensions/stars/stars.py
+++ extensions/stars/stars.py
@@ -2,6 +2,7 @@ import disnake
 from disnake.ext import commands
 from bot import CBCBot
 from typing import List
+from datetime import timedelta
 
 
 class Stars(commands.Cog):
@@ -14,7 +15,7 @@ class Stars(commands.Cog):
     async def on_raw_reaction_add(self, event: disnake.RawReactionActionEvent):
         if self.first_run:
             repost_channel = self.bot.get_channel(self.bot.env.STARS_REPOST_CHANNEL_ID)
-            messages = await repost_channel.history(limit=10).flatten()
+            messages = await repost_channel.history(limit=25).flatten()
             for message in messages:
                 if len(message.embeds) > 0:
                     first_embed = message.embeds[0]
@@ -26,59 +27,60 @@ class Stars(commands.Cog):
             and event.channel_id == self.bot.env.STARS_VOTE_CHANNEL_ID
             and event.message_id not in self.reposted
         ):
-                channel = self.bot.get_channel(event.channel_id)
-                message = await channel.fetch_message(event.message_id)
-                if message.author == self.bot.user:
-                    return
-                reactions = message.reactions
-                for reaction in reactions:
-                    if reaction.emoji == "⭐" and (
-                        (
-                            reaction.count >= self.bot.env.STARS_THRESHOLD
-                            and (message.embeds != None or message.attachments != None)
+            channel = self.bot.get_channel(event.channel_id)
+            message = await channel.fetch_message(event.message_id)
+            if message.author == self.bot.user:
+                return
+            reactions = message.reactions
+            for reaction in reactions:
+                if reaction.emoji == "⭐" and (
+                    (
+                        reaction.count >= self.bot.env.STARS_THRESHOLD
+                        and message.created_at > disnake.utils.utcnow() - timedelta(days=7)
+                        and (message.embeds != None or message.attachments != None)
+                    )
+                    or await reaction.users().find(lambda u: u.id in self.bot.env.STARS_BYPASS) != None
+                ):
+                    # repost the message within an embed to the repost channel
+                    repost_channel = self.bot.get_channel(self.bot.env.STARS_REPOST_CHANNEL_ID)
+
+                    first_embed = (
+                        disnake.Embed(
+                            color=disnake.Colour.gold(),
+                            url=message.jump_url,
                         )
-                        or await reaction.users().find(lambda u: u.id in self.bot.env.STARS_BYPASS) != None
-                    ):
-                        # repost the message within an embed to the repost channel
-                        repost_channel = self.bot.get_channel(self.bot.env.STARS_REPOST_CHANNEL_ID)
+                        .set_author(name=message.author.display_name, icon_url=message.author.avatar.url)
+                        .set_footer(text=event.message_id)
+                        .add_field(
+                            name="Original message",
+                            value=f"[Jump to message]({message.jump_url})",
+                            inline=False,
+                        )
+                    )
 
-                        first_embed = (
-                            disnake.Embed(
-                                color=disnake.Colour.gold(),
-                                url=message.jump_url,
-                            )
-                            .set_author(name=message.author.display_name, icon_url=message.author.avatar.url)
-                            .set_footer(text=event.message_id)
-                            .add_field(
-                                name="Original message",
-                                value=f"[Jump to message]({message.jump_url})",
-                                inline=False,
-                            )
+                    if message.content != None and message.content != "":
+                        first_embed.add_field(
+                            name="Message",
+                            value=message.content,
                         )
 
-                        if message.content != None and message.content != "":
-                            first_embed.add_field(
-                                name="Message",
-                                value=message.content,
-                            )
+                    if len(message.attachments) > 0:
+                        for i, attachment in enumerate(message.attachments):
+                            first_embed.add_field(name=f"Attachment {i + 1}", value=attachment.url, inline=False)
+                        for i, attachment in enumerate(message.attachments):
+                            if attachment.content_type != None and attachment.content_type.startswith("image"):
+                                first_embed.set_image(url=message.attachments.pop(i).url)
+                                break
 
-                        if len(message.attachments) > 0:
-                            for i, attachment in enumerate(message.attachments):
-                                first_embed.add_field(name=f"Attachment {i + 1}", value=attachment.url, inline=False)
-                            for i, attachment in enumerate(message.attachments):
-                                if attachment.content_type != None and attachment.content_type.startswith("image"):
-                                    first_embed.set_image(url=message.attachments.pop(i).url)
-                                    break
+                    embeds, other_attachments = await self.make_image_embeds(message.attachments)
+                    embeds.insert(0, first_embed)
 
-                        embeds, other_attachments = await self.make_image_embeds(message.attachments)
-                        embeds.insert(0, first_embed)
-
-                        try:
-                            await repost_channel.send(embeds=embeds)
-                            if other_attachments != None and other_attachments != []:
-                                await repost_channel.send(files=other_attachments)
-                        finally:
-                            self.reposted.add(event.message_id)
+                    try:
+                        await repost_channel.send(embeds=embeds)
+                        if other_attachments != None and other_attachments != []:
+                            await repost_channel.send(files=other_attachments)
+                    finally:
+                        self.reposted.add(event.message_id)
 
     async def make_image_embeds(
         self,
